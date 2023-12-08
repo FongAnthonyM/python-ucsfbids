84cfda55fe98ee99a9fd1e89f40628e17eaa8cf1
Raphaël Christin <70120121+raphaelchristin@users.noreply.github.com>
2023-12-07 16:42:53 -0800

    update upenn-export and debug printing

--- src/ucsfbids/datasets/exporters/datasetupennexporter.py
+++ src/ucsfbids/datasets/exporters/datasetupennexporter.py
@@ -6,9 +6,11 @@ from ucsfbids.subjects.exporters.subjectupennexporter import SubjectUPENNExporte
 
 class DatasetUPENNExporter(DatasetBIDSExporter):
     def export_subjects(self, path: Path, sub_name_map: dict[str, str]):
+        print("using new version")
         assert self.dataset is not None
         for subject in self.dataset.subjects.values():
             subject.add_exporter("UPENN", SubjectUPENNExporter)
             assert subject.name is not None
+            print(f"Now exporting subject {subject.name}")
             new_name = sub_name_map[subject.name]
             subject.create_exporter("UPENN").execute_export(path, new_name)
